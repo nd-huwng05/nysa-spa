3f8e1d74456e387e2df09e303b3b3c7f95c54c6d
nd-huwng05 <nguyendinhhung290605@gmail.com>
2025-12-05 22:49:56 +0700

    update

--- app/modules/home/__init__.py
+++ app/modules/home/__init__.py
@@ -5,6 +5,7 @@ from .routes.controller import Controller
 from .service.service import Service
 from .routes import register_routes
 from app.core.environment import Environment
+from ..service import ServiceModule
 
 
 class HomeModule:
@@ -14,7 +15,7 @@ class HomeModule:
         self.env = env
         repo = Repository(env)
         self.service = Service(repo=repo, config=self.config)
+        self.service_module = ServiceModule(app=app, env=env)
 
     def register(self):
         register_routes(self.app, self.service, self.config)
-
--- app/modules/home/repository/models.py
+++ app/modules/home/repository/models.py
@@ -1,4 +1,26 @@
-from sqlalchemy import Column
+from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy.orm import relationship
 
 from app.core.database import BaseModel
+
+class Section(BaseModel):
+    __tablename__ = 'section'
+    name = Column(String(150), nullable=False)
+    description = Column(Text)
+    contents = relationship("SectionContent", back_populates="section", cascade="all, delete-orphan")
+
+class SectionContent(BaseModel):
+    __tablename__ = 'section_content'
+    section_id = Column(Integer, nullable=False)
+    key = Column(String(150), nullable=False)
+    value = Column(Text)
+
+class Event(BaseModel):
+    __tablename__ = 'event'
+    title = Column(String(255), nullable=False)
+    description = Column(Text)
+    start = Column(DateTime, nullable=False)
+    end = Column(DateTime, nullable=False)
+    details_url = Column(Text)
+
 
