cd49d8234c17aeea5c7a7cdcf04d3317ae70fc5d
MiQua102 <MixQua0205@gmail.com>
2025-12-09 23:37:53 +0700

    update

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = "secret"
 
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("PASSWORD", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("PORT", "3306")
+DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "nysadb")
 
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
