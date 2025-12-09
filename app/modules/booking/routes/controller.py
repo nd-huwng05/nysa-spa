35a24400cb2bb65bfa1c36b3a7409d2668d0b2de
dangnhatnam <Dangnem03@gmail.com>
2025-12-10 00:40:44 +0700

    add booking interface1
    # Conflicts:
    #	app/__init__.py
    #	app/modules/booking/__init__.py
    #	app/modules/booking/routes/__init__.py
    #	app/modules/booking/routes/controller.py
    #	app/modules/home/routes/controller.py

--- app/modules/booking/routes/controller.py
+++ app/modules/booking/routes/controller.py
@@ -1,11 +1,20 @@
 from flask import request, render_template, redirect, url_for, flash, session
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+
+from app.core.environment import Environment
+from app.extensions import oauth
+from ..config.config_module import ModuleConfig
+from ..service.service import Service
 from .handler import Handler
 
 class Controller:
     def __init__(self, config, service, env):
         self.handler = Handler(config, service, env)
 
-    def index(self):
-        return render_template('page/service.html')
+    @staticmethod
+    def book_view(self):
+        return render_template('page/book_view.html')
 
-
+    @staticmethod
+    def book_confirm(self):
+        return render_template('page/book_confirm.html')
