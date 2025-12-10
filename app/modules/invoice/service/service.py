4b598f23fc11392c18b8e61d7e4c80877a064c7e
nd-huwng05 <nguyendinhhung290605@gmail.com>
2025-12-11 01:47:31 +0700

    fix model

--- app/modules/invoice/service/service.py
+++ app/modules/invoice/service/service.py
@@ -1,5 +1,8 @@
+from ..repository.repo import Repository
+
+
 class Service:
-    def __init__(self, repo, config):
+    def __init__(self, repo:Repository, config):
         self.repo = repo
         self.config = config
 
--- app/modules/voucher/routes/handler.py
+++ app/modules/voucher/routes/handler.py
@@ -1,5 +1,7 @@
+from ..service.service import Service
+
 class Handler:
-    def __init__(self, config, service, env):
+    def __init__(self, config, service:Service, env):
         self.config = config
         self.service = service
         self.env = env
