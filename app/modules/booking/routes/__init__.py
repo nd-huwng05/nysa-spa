be251f43fe9f850ae280e277bfa980989e5bcc76
nd-huwng05 <nguyendinhhung290605@gmail.com>
2025-12-11 10:51:32 +0700

    update

--- app/modules/booking/routes/__init__.py
+++ app/modules/booking/routes/__init__.py
@@ -6,10 +6,10 @@ from .controller import Controller
 def register_routes(app, service, config:ModuleConfig, env:Environment):
     c = Controller(service=service, config=config, env=env)
 
-    booking_routes = Blueprint('book', __name__, template_folder='../templates', static_folder='../static', url_prefix='/book')
+    booking_routes = Blueprint('booking', __name__, template_folder='../templates', static_folder='../static', url_prefix='/booking')
 
 
-    booking_routes.add_url_rule('/book-view', view_func=c.book_view, methods=['GET'])
+    booking_routes.add_url_rule('/booking-view', view_func=c.book_view, methods=['GET'])
     # booking_routes.add_url_rule('/book-confirm', view_func=c.book_confirm(), methods=['GET'])
 
     app.register_blueprint(booking_routes)
